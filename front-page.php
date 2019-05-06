e89eb8fc8e0233ab2edc28cb7e8a9791a60e2b3f
Unknown <v-JoshuaStewart@smarttech.com>
2019-05-06 11:40:15 +0200

    Adding bootstrap

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
- <main id="main" class="site-main">
+ <main id="main" class="site-main container">
  <?php
  // the loop
  while ( have_posts() ) : the_post();
